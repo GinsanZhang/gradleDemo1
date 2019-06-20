ca3573be53e12720c22bc30173dab9dbde5df62e
ginsan <zerozoogle@gmail.com>
2019-06-20 20:22:09 +0800

    add  gitignore

--- src/main/java/com/ginsan/shape/Rectangle.java
+++ src/main/java/com/ginsan/shape/Rectangle.java
@@ -1,19 +1,19 @@
 package com.ginsan.shape;
 
 public class Rectangle {
-    private int length ;
+    private int height ;
     private int width;
 
     public Rectangle(int length, int width) {
-        this.length = Integer.max(length,0);
+        this.height = Integer.max(length,0);
         this.width = Integer.max(width,0);
     }
 
     public int getPerimeter(){
-        return (length+width)*2;
+        return (height+width)*2;
     }
     public int getArea(){
-        return length*width;
+        return height*width;
     }
 
 }
